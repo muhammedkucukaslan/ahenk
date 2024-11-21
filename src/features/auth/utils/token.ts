fbb6814c34eac9ae3b0c5e34afda881b98c5de52
Muhammed Küçükaslan <kucukaslanmuhammed852@gmail.com>
2024-11-21 13:49:47 +0300

    update: getJWTSecretKey functin shifted to returnFunctions

--- src/features/auth/utils/token.ts
+++ src/features/auth/utils/token.ts
@@ -1,24 +1,5 @@
-import { jwtVerify ,SignJWT } from "jose"
-
-export const getJWTSecretKey = () => {
-    const secretKey = process.env.NEXT_PUBLIC_JWT_SECRET_KEY
-    if (!secretKey) {
-        throw new Error("JWT_SECRET is not defined")
-    }
-    return new TextEncoder().encode(secretKey)
-}
-
-export async function verifyToken(token : string)  {
-
-    try {
-        const payload = await jwtVerify(token, getJWTSecretKey())
-        return payload 
-    } catch (error) {
-        console.error('error', error)
-        return null
-    }
-}
-
+import { SignJWT } from "jose"
+import { getJWTSecretKey } from "@/src/utils/returnFunctions"
 
 export async function generateToken(id: string, role: string ) : Promise<string> {
     
