a25f6309c29b789f92b35589fa77c77fe27daae2
Muhammed Küçükaslan <kucukaslanmuhammed852@gmail.com>
2024-11-22 23:34:20 +0300

    add: logout route and controller

---
+++ src/app/api/(auth)/logout/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest } from "next/server";
+import { AuthController } from "@/src/app/controllers/auth-controller";
+
+
+export async function POST(req: NextRequest) {
+    return AuthController.signup(req);
+}
