5ed92c919e13ae0437b7a4f738d28210a8640527
Abdulrahman Saka <c.abdulrahmansaka@gmail.com>
2024-11-23 00:08:41 +0300

    Edit: Signup form

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,79 +1,21 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import SignupFrom from "@/src/features/auth/forms/Signup-from";
+import { CardContent, CardFooter, CardHeader } from "@/src/components/ui/card";
+import React from "react";
 
 const SignupPage = () => {
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const router = useRouter();
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const res = await fetch('/api/signup', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email,
-        name,
-        password,
-      }),
-    });
-
-    if (res.ok) {
-      // Başarılı olduğunda yönlendirme yapabilirsin
-      router.push('/dashboard'); // Örneğin bir welcome sayfasına yönlendirme yapabilir
-    } else {
-      const errorData = await res.json();
-      setError(errorData.message || 'Signup failed');
-    }
-  };
-
   return (
     <div>
-      <h1>Signup</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            style={{color : 'green'}}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            style={{color : 'green'}}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            style={{color : 'green'}}
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        <button type="submit">Signup</button>
-      </form>
+      <CardHeader>Hosgeldiniz</CardHeader>
+      <CardContent>
+        <SignupFrom />
+      </CardContent>
+      <CardFooter>
+        <p>
+          Hesabın var mı? <a href="/login">giris yap</a>
+        </p>
+      </CardFooter>
     </div>
   );
 };
