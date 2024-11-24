8d5c46b6ad26e1a5b87a8bdc825715fe13dd6130
Muhammed Küçükaslan <kucukaslanmuhammed852@gmail.com>
2024-11-24 07:57:32 +0300

    fix(middleware): remove useless logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { getUserFromCookies, redirectToLogin, isAuthPage, verifyToken } from '@/src/utils/returnFunctions';
-import next from 'next';
+import { getUserFromCookies, redirectToLogin, isAuthPage } from '@/src/utils/returnFunctions';
 
 
 export async function middleware(request: NextRequest) {
     const { url, nextUrl } = request;
     const {success,data} = await getUserFromCookies(request);
-    console.log("success", success);
-    console.log("data", data);
     const isAuthPageRequest = isAuthPage(nextUrl.pathname);
-    console.log("isAuthPageRequest", isAuthPageRequest);
     if (isAuthPageRequest) {
         if (!success) {
             const response = NextResponse.next();
             response.cookies.delete("token");
             return response;
         }
-        console.log("burasi calisti B")
         const response = NextResponse.redirect(new URL('/dashboard', url));
         return response;
     }
@@ -35,7 +30,6 @@ export async function middleware(request: NextRequest) {
     }
     
     const response = NextResponse.next();
-    response.headers.set('muhammed', "muhammed");
     response.headers.set('x-user-id', data.id);
     response.headers.set('x-user-role', data.role);
     return response
