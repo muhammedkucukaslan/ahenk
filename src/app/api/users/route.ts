b3b9f8017433b264dc0e509af771bcfa9f1708cd
Muhammed Küçükaslan <kucukaslanmuhammed852@gmail.com>
2024-11-24 09:57:48 +0300

    fix(controllers): adjust user controllers

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -3,11 +3,5 @@ import { UserController } from "../../controllers/user-controller";
 
 
 export async function GET(req: NextRequest) {
-    try {
         return UserController.getUser(req)
-    } catch (error) {
-        console.error("Error:", error)
-        return NextResponse.json({ "error": "Failed to get user" })
-    }
-
 }
