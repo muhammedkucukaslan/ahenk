f684b19d3ad0767b07b6a99ff77e5ad46169a799
Muhammed Küçükaslan <kucukaslanmuhammed852@gmail.com>
2024-11-27 17:10:29 +0300

    feat(schema): update user

---
+++ prisma/migrations/20241127055627_add_profile_pic_and_bio_2_user/migration.sql
@@ -0,0 +1,18 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "profilePic" TEXT NOT NULL DEFAULT 'https://cdn.iconscout.com/icon/free/png-256/avatar-380-456332.png',
+    "bio" TEXT,
+    "role" TEXT NOT NULL DEFAULT 'USER'
+);
+INSERT INTO "new_User" ("email", "id", "name", "password", "role") SELECT "email", "id", "name", "password", "role" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
