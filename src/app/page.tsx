e91e9ef98c7e5456bedeba3ded41baaead8eb880
mr-isik <faruk_isik_24@protonmail.com>
2024-12-03 12:15:40 +0300

    Update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import FeedContainer from '../features/posts/components/feed-container';
-import PopularCommunities from '../features/groups/components/popular-communities';
 
 export default function Home() {
   return (
     <>
       <FeedContainer />
-      <PopularCommunities />
     </>
   );
 }
