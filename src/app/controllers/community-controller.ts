a41cdcf086cd3abae3e81870f946ff03a476567b
muhammedkucukaslan <kucukaslanmuhammed852@gmail.com>
2024-12-14 17:04:59 +0300

    feat: add controllers

--- src/app/controllers/community-controller.ts
+++ src/app/controllers/community-controller.ts
@@ -1,56 +1,192 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { createResult } from '@/src/utils/returnFunctions';
+import { handleErrorResponse, handleSuccessResponse, validateData } from '@/src/utils/returnFunctions';
 import { createCommunitySchema } from '@/src/features/communities/validation';
 import { CommunityService } from '@/src/features/communities/server/service';
-export const CommunityController = {
-    createCommunity: async (req: NextRequest): Promise<ResultResponse<null>> => {
+
+export const CommunityController: ICommunityController = {
+    getCommunities: async (req: NextRequest): Promise<IResponse> => {
         try {
-            const leaderId = req.headers.get('x-user-id');
-            if (!leaderId) {
-                return NextResponse.json(createResult(false, null, "Yetkilendirilmemiş işlem"), { status: 401 });
-            }
-            const { name, isPublic, description } = await req.json();
-
-            await createCommunitySchema.validate({ name, leaderId, isPublic, description });
-
-            const result = await CommunityService.createCommunity({ name, leaderId, isPublic, description });
-
+            const result = await CommunityService.getCommunities();
             if (!result.success) {
-                return NextResponse.json(createResult(false, null, result.message || "Topluluk oluşturulamadı"), { status: 400 });
+                return handleErrorResponse(result.ERR_CODE, result.message);
             }
-
-            return NextResponse.json(createResult(true, null), { status: 201 });
-        } catch (error: any) {
-            console.error("Create Community Error:", error);
-            if (error.name === 'ValidationError') {
-                return NextResponse.json(createResult(false, null, "Invalid data"), { status: 400 });
-            }
-            return NextResponse.json(createResult(false, null, "Topluluk oluşturulurken hata oluştu"), { status: 500 });
+            return handleSuccessResponse(result.data);
+        } catch (error) {
+            return handleErrorResponse("SERVER_ERROR", "Topluluklar getirilirken bir hata oluştu");
         }
     },
-    getCommunityById: async (req: NextRequest, communityId: string): Promise<ResultResponse<null>> => {
+    createCommunity: async (req: NextRequest): Promise<IResponse> => {
+        try {
+            const userId = req.headers.get('x-user-id');
+            if (!userId) {
+                return handleErrorResponse("UNAUTHORIZED", "Yetkilendirme hatası");
+            }
+            const { name, isPublic, description, topics } = await req.json();
+
+            const validatedData = await validateData({ name, leaderId: userId, isPublic, description, topics }, createCommunitySchema);
+            if (!validatedData) {
+                return handleErrorResponse("BAD_REQUEST", "Invalid data")
+            }
+
+            const result = await CommunityService.createCommunity({ name, leaderId: userId, isPublic, description, topics });
+            if (!result.success) {
+                return handleErrorResponse(result.ERR_CODE!, result.message!)
+            }
+            return handleSuccessResponse(null, 201);
+        } catch (error: any) {
+            console.error("Create Community Error:", error);
+            return handleErrorResponse("SERVER_ERROR", "Topluluk oluşturulurken bir hata oluştu");
+        }
+    },
+    updateCommunity: async (req: NextRequest, communityName: string): Promise<IResponse> => {
         try {
             const clientId = req.headers.get('x-user-id');
             if (!clientId) {
-                return NextResponse.json(createResult(false, null, "Yetkilendirilmemiş işlem"), { status: 401 });
+                return handleErrorResponse("UNAUTHORIZED", "Yetkilendirme hatası");
             }
-            const isPermisible = await CommunityService.checkCommunityPermission(communityId, clientId);
-            if (!isPermisible.success) {
-                if (isPermisible.message) {
-
-                    return NextResponse.json(createResult(false, null, isPermisible.message), { status: 400 });
-                }
-                const limitedResult = await CommunityService.getLimitedCommunityById(communityId);
-                return NextResponse.json(createResult(true, { isPermisible: false, ...limitedResult.data}), { status: 200 });
-            }
-            const result = await CommunityService.getCommunityById(communityId);
+            const body = await req.json() as ICommunityUpdation
+            const result = await CommunityService.updateCommunity(communityName, clientId, body);
             if (!result.success) {
-                return NextResponse.json(createResult(false, null, result.message || "Topluluk bulunamadı"), { status: 404 });
+                return handleErrorResponse(result.ERR_CODE, result.message);
             }
-            return NextResponse.json(createResult(true, { isPermisible: true , ...result.data}), { status: 200 });
+            return handleSuccessResponse(null, 201);
+
         } catch (error) {
-        console.error("Get Community Error:", error);
-            return NextResponse.json(createResult(false, null, "Topluluk bilgileri getirilirken hata oluştu"), { status: 500 });
+            return handleErrorResponse("SERVER_ERROR", "Topluluk güncellenirken bir hata oluştu");
+        }
+    },
+    updatePartialPartOfCommunity: async (req: NextRequest, communityName: string): Promise<IResponse> => {
+        try {
+            const clientId = req.headers.get('x-user-id');
+            if (!clientId) {
+                return handleErrorResponse("UNAUTHORIZED", "Yetkilendirme hatası");
+            }
+            const url = new URL(req.url);
+            const field = url.searchParams.get('field')
+            const body = await req.json();
+            const result = await CommunityService.updatePartialPartOfCommunity(communityName, clientId, body, field!);
+            if (!result.success) {
+                return handleErrorResponse(result.ERR_CODE, result.message);
+            }
+            return handleSuccessResponse(null, 201);
+        } catch (error) {
+            return handleErrorResponse("SERVER_ERROR", "Topluluk güncellenirken bir hata oluştu");
+        }
+    },
+    getCommunityByName: async (req: NextRequest, communityName: string): Promise<IResponse> => {
+        try {
+            const clientId = req.headers.get('x-user-id');
+            const result = await CommunityService.getCommunityByName(communityName, clientId!);
+            if (!result.success) {
+                return handleErrorResponse(result.ERR_CODE, result.message);
+            }
+            return handleSuccessResponse(result.data);
+        } catch (error) {
+            return handleErrorResponse("SERVER_ERROR", "Topluluk bilgileri getirilirken bir hata oluştu");
+        }
+    },
+    getMembers: async (req: NextRequest, communityName: string): Promise<IResponse> => {
+        try {
+            const clientId = req.headers.get('x-user-id');
+            const result = await CommunityService.getMembers(communityName, clientId!);
+            if (!result.success) {
+                return handleErrorResponse(result.ERR_CODE, result.message);
+            }
+            return handleSuccessResponse(result.data);
+        } catch (error) {
+            return handleErrorResponse("SERVER_ERROR", "Üyeler getirilirken bir hata oluştu");
+        }
+    },
+    joinCommunity: async (req: NextRequest, communityName: string): Promise<IResponse> => {
+        try {
+            const userId = req.headers.get('x-user-id');
+            if (!userId) {
+                return handleErrorResponse("UNAUTHORIZED", "Yetkilendirme hatası");
+            }
+            const result = await CommunityService.joinCommunity(communityName, userId);
+            if (!result.success) {
+                return handleErrorResponse(result.ERR_CODE, result.message);
+            }
+            return handleSuccessResponse(null, 201);
+        } catch (error) {
+            return handleErrorResponse("SERVER_ERROR", "Topluluğa katılırken bir hata oluştu");
+        }
+    },
+    addMember: async (req: NextRequest, communityName: string, memberId: string): Promise<IResponse> => {
+        try {
+            const clientId = req.headers.get('x-user-id');
+            if (!clientId) {
+                return handleErrorResponse("UNAUTHORIZED", "Yetkilendirme hatası");
+            }
+            const result = await CommunityService.addMember(communityName, clientId, memberId);
+            if (!result.success) {
+                return handleErrorResponse(result.ERR_CODE, result.message);
+            }
+            return handleSuccessResponse(null, 201);
+        } catch (error) {
+            return handleErrorResponse("SERVER_ERROR", "Üye eklenirken bir hata oluştu");
+        }
+    },
+    removeMember: async (req: NextRequest, communityName: string, memberId: string): Promise<IResponse> => {
+        try {
+            const clientId = req.headers.get('x-user-id');
+            if (!clientId) {
+                return handleErrorResponse("UNAUTHORIZED", "Yetkilendirme hatası");
+            }
+            const result = await CommunityService.removeMember(communityName, clientId, memberId);
+            if (!result.success) {
+                return handleErrorResponse(result.ERR_CODE, result.message);
+            }
+            return handleSuccessResponse(null, 201);
+        } catch (error) {
+            return handleErrorResponse("SERVER_ERROR", "Üye silinirken bir hata oluştu");
+        }
+    },
+    promoteMember: async (req: NextRequest, communityName: string, memberId: string): Promise<IResponse> => {
+        try {
+            const clientId = req.headers.get('x-user-id');
+            if (!clientId) {
+                return handleErrorResponse("UNAUTHORIZED", "Yetkilendirme hatası");
+            }
+
+            const result = await CommunityService.promoteMember(communityName, clientId, memberId);
+            if (!result.success) {
+                return handleErrorResponse(result.ERR_CODE, result.message);
+            }
+            return handleSuccessResponse(null);
+        } catch (error) {
+            return handleErrorResponse("SERVER_ERROR", "Lider eklenirken bir hata oluştu");
+        }
+    },
+    demoteMember: async (req: NextRequest, communityName: string, memberId: string): Promise<IResponse> => {
+        try {
+            const clientId = req.headers.get('x-user-id');
+            if (!clientId) {
+                return handleErrorResponse("UNAUTHORIZED", "Yetkilendirme hatası");
+            }
+
+            const result = await CommunityService.demoteMember(communityName, clientId, memberId);
+            if (!result.success) {
+                return handleErrorResponse(result.ERR_CODE, result.message);
+            }
+            return handleSuccessResponse(null);
+        } catch (error) {
+            return handleErrorResponse("SERVER_ERROR", "Lider kaldırılırken bir hata oluştu");
         }
     }
+}
+
+
+interface ICommunityController {
+    getCommunities: (req: NextRequest) => Promise<IResponse>;
+    createCommunity: (req: NextRequest) => Promise<IResponse>;
+    getCommunityByName: (req: NextRequest, communityName: string) => Promise<IResponse>;
+    getMembers: (req: NextRequest, communityName: string) => Promise<IResponse>;
+    updatePartialPartOfCommunity: (req: NextRequest, communityName: string) => Promise<IResponse>;
+    updateCommunity: (req: NextRequest, communityName: string) => Promise<IResponse>;
+    joinCommunity: (req: NextRequest, communityName: string) => Promise<IResponse>;
+    addMember: (req: NextRequest, communityName: string, memberId: string) => Promise<IResponse>;
+    removeMember: (req: NextRequest, communityName: string, memberId: string) => Promise<IResponse>;
+    promoteMember: (req: NextRequest, communityName: string, memberId: string) => Promise<IResponse>;
+    demoteMember: (req: NextRequest, communityName: string, memberId: string) => Promise<IResponse>;
 }
